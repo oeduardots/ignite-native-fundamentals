085c2fc178b7270497a1212dc7385283a8f32e8a
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-27 17:18:26 -0300

    utilizando stylesheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>Nome do evento</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16
-      }}>Sexta, 27 de Maio de 2024</Text>
+      <Text style={styles.eventDate}>Sexta, 27 de Maio de 2024</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+})
