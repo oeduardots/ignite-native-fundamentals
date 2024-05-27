d5488d7d73ed703df905b87ea65d447aaf36ebe6
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-27 16:12:44 -0300

    estilizando elementos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,22 @@ import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>React Native</Text>
-      <Text>Tela 1</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>Nome do evento</Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>Sexta, 27 de Maio de 2024</Text>
     </View>
   )
 }
