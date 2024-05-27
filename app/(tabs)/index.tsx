6f99f470416a09d3928d65c6d90c811d00ebe107
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-27 17:46:24 -0300

    input de dados

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native"
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 27 de Maio de 2024
+      </Text>
 
-      <Text style={styles.eventDate}>Sexta, 27 de Maio de 2024</Text>
+      <TextInput 
+        style={styles.input} 
+        placeholder="Nome do participante" 
+        placeholderTextColor="#6b6b6b"
+      />
     </View>
   )
 }
@@ -13,17 +22,25 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131016',
+    backgroundColor: "#131016",
     padding: 24
   },
   eventName: {
-    color: '#FDFCFE',
+    color: "#ffffff",
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 48
   },
   eventDate: {
-    color: '#6B6B6B',
+    color: "#6B6B6B",
+    fontSize: 16
+  },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#ffffff",
+    padding: 16,
     fontSize: 16
   }
 })
