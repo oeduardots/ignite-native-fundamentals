61d286783e5da4e5a1d8392803e3d0f342e2b814
Eduardo Tavares <edutav.dev@gmail.com>
2024-05-28 22:46:15 -0300

    criando form

---
+++ app/(home)/index.tsx
@@ -0,0 +1,34 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
+
+import { styles } from './styles'
+
+export default function HomeScreen() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 27 de Maio de 2024
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante" 
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
